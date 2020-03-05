923694d2d4eff79e7a0be355490bb24b8e024f84
4rfel <rafael_1999_@hotmail.com.br>
2020-03-05 11:28:18 -0300

    entrega sexta

--- poly.py
+++ poly.py
@@ -6,23 +6,23 @@ def calc_matriz_rigidez(E, A, L, M, quantElementos):
 def mount_M(F):
     m0 = np.array([[1, -1], [-1, 1]])
     M = np.zeros([len(F), len(F)])
-    for i in range(M.shape[0]):
-        for j in range(M.shape[1]):
-            if(i==j and i!=M.shape[0]-1 and j!=M.shape[1]-1):
-                M[i:i+2, i:i+2] += m0
+    for i in range(M.shape[0]-1):
+        M[i:i+2, i:i+2] += m0
+    
     return M
 
 def cond_contorno(F, E, A, L, vu):
     row = 0
     col = 1
-    a = 0
+    a = []
     M = mount_M(F)
-    
-    for i in range(len(vu)):
+    for i in range(vu.shape[0]):
         if vu[i] == 0:
-            F = np.delete(F, obj=i-a, axis=row)
-            M = np.delete(np.delete(M, obj=i-a, axis=row), obj=i-a, axis=col)
-        a = i
+            a.append(i)    
+    F = np.delete(F, obj=a, axis=row)
+    M = np.delete(M, obj=a, axis=row)
+    M = np.delete(M, obj=a, axis=col)
+
     quantElementos = len(vu)-1
     mr = calc_matriz_rigidez(E, A, L, M, quantElementos)
     vu = np.linalg.solve(mr, F)
@@ -35,24 +35,25 @@ def calc_reacao_apoio(E, A, L, vu):
     
 def remount_vu(vu, vuf):
     v = []
+    k = 0
     for i in vu:
         if i == 0:
             v.append(0)
         else:
-            v.append(vuf[0])
-            vuf = np.delete(vuf, obj=0, axis=0)
+            v.append(vuf[k])
+            k += 1;
     return np.array(v)
 
 def calc_deformacao(L, vu, ):
     d = []
     for i in range(len(vu)-1):
         d.append((1/(L/(len(vu)-1)))*np.matmul(np.array([-1, 1]), np.array([vu[i], vu[i+1]])))
-    d = np.array(d).reshape((len(d), 1))
+    d = np.array(d)
     return d
         
 def calc_tensao(E, d):
     return E*d
-
+    
 
 L = 2             # tamanho inicial (m)
 A = 0.02          # area (m**2)
@@ -61,7 +62,6 @@ P = 50 * 10**3    # forca (N)
 
 F = np.array([0, 0, P])
 vu = np.array([0, 1, 1])
-
 
 vuf = cond_contorno(F, E, A, L, vu)
 vu = remount_vu(vu, vuf)
