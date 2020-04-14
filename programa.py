ffcfbfea23968f0ed58df156d70d0cb9e010ca4c
Arfel <rafael_1999_@hotmail.com.br>
2020-04-13 21:06:53 -0300

    att valores de ruptura

--- programa.py
+++ programa.py
@@ -3,7 +3,7 @@ from ponte import Ponte
 from viga import Viga
 import numpy as np
 
-p = 0
+p = 1
 nn, N, nm, Inc, nc, F, nr, R = ft.importa("ponte.xlsx")
 if p:
     ft.plota_ponte(N, Inc)  
@@ -47,9 +47,10 @@ else:
 
 f_interna = ponte.calc_f(t)
 
-tensao_ruptura_tracao = 1000000
-tensao_ruptura_compressao = 1000000
+tensao_ruptura_tracao = 18e6
+tensao_ruptura_compressao = 18e6
 
 c = ponte.testa_colapso(tensao_ruptura_tracao, tensao_ruptura_compressao, t, d, vu, ponte.vu)
+print(c)
 ft.geraSaida("ponte", reacao.reshape([len(reacao), 1]), ponte.vu.reshape([len(ponte.vu), 1]), d.reshape([len(d), 1]), f_interna.reshape([len(f_interna), 1]), t.reshape([len(t), 1]))
 
